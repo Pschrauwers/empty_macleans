e3aefbdf8f51509da55be554cec0c09da817bb08
vetterteam <vetter@getvetter.com>
2013-01-08 15:43:20 -0500

    updated styles and removed parsing stuff

--- issue_parse/parse.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'open-uri'
-require 'nokogiri'
- 
-xml = Nokogiri(File.read("nitf.xml"))
-xml.encoding = 'UTF-8'
-xslt = Nokogiri::XSLT(File.read("standard_article.xsl"))
-
-puts xslt.transform(xml).to_html
--- issue_parse/toc_parse.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'open-uri'
-require 'nokogiri'
-
-toc_xml = Nokogiri(File.read('master.xml'))
-toc_xslt = Nokogiri::XSLT(File.read("toc.xsl"))
-toc = File.new('toc.html', 'w')
-toc.write(toc_xslt.transform(toc_xml).to_html)
-toc.close
